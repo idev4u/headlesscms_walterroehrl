0dedbcfa3c4e6befacc620d2190961976cc4aa3d
Jannik Wolf <jannik.wolf@porsche.de>
2020-10-14 19:22:14 +0200

    styling changes

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,9 @@ var quotes = require('../services/quotes')
 var date = require('../services/date')
 var schedule = require('node-schedule');
 
-
-/* Probably not needed anymore
-router.use(function(req,res, next) {
-  quotes.getQuotes().then(function (quoteofTheDay) {
-    //run function everyday at midnight: https://stackoverflow.com/questions/51301301/how-would-i-get-a-function-to-run-every-24-hours-on-a-server/51301855
-    //quoteOfTheDay Mitternacht switchen (über env variables fakebar?)
-    //Datum im Frontend anzeigen
-    req.randomQuote = quoteofTheDay.items[Math.floor(Math.random() * quoteofTheDay.items.length)]
-    next()
-  })
-  .catch(function (err) {
-    console.log('quotes.js - getQuotes error:', JSON.stringify(err,null,2))
-    next()
-  })
-})
-*/
-
 //set intial quote after server start
 quotes.getQuotes().then(function (randomQuote) {
   quoteOfTheDay = randomQuote.items[Math.floor(Math.random() * randomQuote.items.length)]
-  //console.log('intitial quote test: '+ randomQuote.items.length)
 })
 .catch(function (err) {
   console.log('quotes.js - getQuotes error:', JSON.stringify(err,null,2))
@@ -32,6 +14,7 @@ quotes.getQuotes().then(function (randomQuote) {
 
 //cron job to reroll the quote of the day once a day at midnight
 //scheduleJob: seperated stars/numbers are corresponding second(optional)/minute/hour/day of month/month/day of week
+//for test cases cron job rythm set to 1 minute: schedule.scheduleJob('0 * * * * *', function(){...}
 schedule.scheduleJob('0 0 * * *', function(){
   quotes.getQuotes().then(function (randomQuote) {
     quoteOfTheDay = randomQuote.items[Math.floor(Math.random() * randomQuote.items.length)]
--- services/date.js
+++ services/date.js
@@ -1,14 +1,15 @@
 
 function getDate() {
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+  var today = new Date()
+  //format into dd.mm.yyyy
+  var dd = String(today.getDate()).padStart(2, '0')
+  var mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
+  var yyyy = today.getFullYear()
 
-today = dd + '.' + mm + '.' + yyyy;
-return today
+  today = dd + '.' + mm + '.' + yyyy
+  return today
 }
 
-  module.exports = {
-    getDate
-  }
+module.exports = {
+  getDate
+}
